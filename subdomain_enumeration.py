04b83c2cbd64ee13d78be564e4922d70e7e68313
Seyit Bahadir Cetin <34353055+bahadircetin@users.noreply.github.com>
2020-05-14 16:33:34 +0300

    General Improvement
    Update: General function auto_recon now seperated in several functions (sublist3r, recon_ng, the_harvester) in order to increase modularity of the code. Also, main() function included for the execution and some bugs fixed in the existing functions.
    
    New Features: Argparse for terminal execution. Options added "-d" for target domain and "-o" for output file path.
    
    New Functions:
    merge_lists(): It takes all output files come from the tools and merge them into one file  without duplicates. As a result, it creates a one unique subdomain list.
    write_csv(): It writes the dictionary returned from ns_lookup() into a CSV file. This CSV is the result file of the program. It takes result path which is given by user.

--- subdomain_enumeration.py
+++ subdomain_enumeration.py
@@ -1,21 +1,44 @@
 import os
-import sys
 import subprocess
+import argparse
+from collections import OrderedDict
+
+"""Argparse for terminal execution"""
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--domain", required=True, help="Target domain to search.")
+ap.add_argument("-o", "--output", required=True, help="Output file path to write results into a CSV file.")
+args = vars(ap.parse_args())
+"""END argparse for terminal execution"""
 
 
-def auto_recon(target_domain):
+def main(target, result_path):
+    """Scan operations"""
+    sublist3r(target)
+    the_harvester(target)
+    recon_ng(target)
+    """End of scan operations"""
+
+    # Formatting the results and make them unique.
+    merge_lists(target)
+    # Writing results into a csv file.
+    write_csv(result_path)
+
+
+def sublist3r(target_domain):
     if os.path.exists('Sublist3r'):
         path = os.path.abspath('Sublist3r')
-        sublister = subprocess.run([path + "./sublist3r.py", "-d", target_domain, "-o", "sublist3r" +
+        sublister = subprocess.run([path + "/./sublist3r.py", "-d", target_domain, "-o", "sublist3r" +
                                     target_domain.split(".")[0] + ".txt"])
         print(sublister)
     else:
         subprocess.run(["git", "clone", "https://github.com/aboul3la/Sublist3r.git"])
         path = os.path.abspath('Sublist3r')
-        sublister = subprocess.run([path + "./sublist3r.py", "-d", target_domain, "-o", "sublist3r" +
+        sublister = subprocess.run([path + "/./sublist3r.py", "-d", target_domain, "-o", "sublist3r" +
                                     target_domain.split(".")[0] + ".txt"])
         print(sublister)
 
+
+def recon_ng(target_domain):
     """For Recon-ng Automatization"""
     # Inserting domain name into recon-ng database
     subprocess.run(["recon-cli", "-C", "\"db insert domains\"" + target_domain])
@@ -27,15 +50,15 @@ def auto_recon(target_domain):
     subprocess.run(["recon-cli", "-m", "brute_hosts", "-x"])
     # Load the reporting module, setting the file name and selecting hosts table from results of recon-ng,
     # then execute the module
-    subprocess.run(["recon-cli", "-m", "reporting/list", "-c", "\"options set FILENAME recon-ng.txt\"",
-                    "-c" "options set TABLE hosts", "-x"])
+    subprocess.run(["sudo", "recon-cli", "-m", "reporting/list", "-c", "options set FILENAME " + os.getcwd() +
+                    "/recon-ng.txt", "-c" "options set TABLE hosts", "-x"])
 
 
-def theharvester(target_domain):
+def the_harvester(target_domain):
     """For theHarvester: The file 'api-keys.yaml' must exist under the directory where the command executed"""
     harvester = subprocess.run(["theHarvester", "-d", target_domain, "-b", "all", "-f", "theharvester_" +
                                 target_domain.split(".")[0] + ".xml"], check=True)
-
+    print(harvester)
     # Returns all subdomains found by theHarvester as a list and saves it to a file
     return the_harvester_parser("theharvester_" + target_domain.split(".")[0] + ".xml", "theharvester_" +
                                 target_domain.split(".")[0] + "_parsed.txt")
@@ -83,10 +106,40 @@ def ns_lookup(subdomain_list):
                         ip_add = row.split(":")[1]
                         ip_dom_dict[dom_name.split("\t")[1]] = ip_add.split()[0]
             except:
-                print("An error occured for " + subdomain.split("\n", 1)[0])
+                print("An error occurred for " + subdomain.split("\n", 1)[0])
 
     print(ip_dom_dict)
     return ip_dom_dict
 
 
-ns_lookup("reconng.txt")
+def merge_lists(target_domain):
+    """It takes all output files which are obtained from tools and merge them in one file without duplicates"""
+    all_lines = ''
+    with open("theharvester_" + target_domain.split(".")[0] + "_parsed.txt", "r") as file1:
+        for subdomain in file1:
+            all_lines += subdomain + "\n"
+    with open("recon-ng.txt", "r") as file2:
+        for subdomain in file2:
+            all_lines += subdomain + "\n"
+    with open("sublist3r" + target_domain.split(".")[0] + ".txt", "r") as file3:
+        for subdomain in file3:
+            all_lines += subdomain + "\n"
+    with open("unique_list.txt", "w") as wp:
+        # Eliminating the duplicate lines.
+        unique_list = "\n".join(list(OrderedDict.fromkeys(all_lines.split("\n"))))
+        for sub_dom in unique_list.splitlines():
+            wp.write("{}\n".format(sub_dom))
+
+
+def write_csv(result_path):
+    """It writes the dictionary returned from ns_lookup() into a csv file with format."""
+    dict_to_write = ns_lookup("unique_list.txt")
+    with open(result_path, "w") as wr:
+        for key in dict_to_write:
+            wr.write("{},{}\n".format(key, dict_to_write[key]))
+
+
+if __name__ == '__main__':
+    main(args["domain"], args["output"])
+
+
